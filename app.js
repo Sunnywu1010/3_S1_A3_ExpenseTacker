7619a12343281e76809dcbef9fb2aa0bd2177094
Sunny <wuszuyu791010@gmail.com>
2022-03-15 16:20:14 +1000

    feat: app init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const port = 3000;
+const bodyParse = require("body-parser");
+const methodOverride = require("method-override");
+const routes = require("./routes");
+require("./config/mongoose");
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.use(bodyParse.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(routes);
+app.listen(port, () => {
+  console.log(`This server is running on http://localhost:${port}`);
+});
