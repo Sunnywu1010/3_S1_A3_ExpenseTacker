ced401eaa114b5a3bcdbfbc44e9a6ca95cacab80
Sunny <wuszuyu791010@gmail.com>
2022-03-30 20:04:21 +1000

    feat: update Model items in order to match lesson

--- models/records.js
+++ models/records.js
@@ -1,34 +1,38 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const recordSchema = new Schema({
-    name: {
-      type:String,
-      required: true,
-    },
-    date: {
-      type:String,
-      required: true,
-    },
-    amount: {
-      type:Number,
-      required: true,
-    },
-    userId: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      index: true,
-      required: true,
-    },
-    categoryId: {
-      type: Schema.Types.ObjectId,
-      ref: "Category",
-      index: true,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-  })
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    index: true,
+    required: true,
+  },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    index: true,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model("Record",recordSchema);
