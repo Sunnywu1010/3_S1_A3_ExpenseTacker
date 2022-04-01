5eb30a1de9382b58bb53edced6795ed01169db20
Sunny <wuszuyu791010@gmail.com>
2022-04-01 13:15:19 +1000

    feat: add categories sorted and totalamount

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Records = require("../../models/records");
 const Categories = require("../../models/categories");
 router.get("/", (req, res) => {
+  let totalAmount = 0
   Records.find()
     .lean()
     .then((records) => {
@@ -11,14 +12,13 @@ router.get("/", (req, res) => {
       records.forEach((record) => {
         const date = record.date.toLocaleDateString(["ban", "id"]);
         record.date=date
+        totalAmount+=record.amount;
       });
       Categories.find()
         .lean()
+        .sort({_id:"asc"})
         .then((categories) => {
-          // const date=categories.date.toDateString()
-
-          // console.log(date)
-          res.render("index", { records, categories });
+          res.render("index", { records, categories, totalAmount });
         });
     })
     .catch((error) => {
