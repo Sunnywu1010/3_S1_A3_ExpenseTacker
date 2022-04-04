53637cf4193b29d123ef5c29f0208b8ea848c5e4
Sunny <wuszuyu791010@gmail.com>
2022-04-04 21:06:28 +1000

    feat: add edit page

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -3,27 +3,41 @@ const router = express.Router();
 const Categories = require("../../models/categories");
 const Records = require("../../models/records");
 
-router.get("/new",(req,res)=>{
-Categories.find()
-  .lean()
-  .sort({ name: "asc" })
-  .then((category) => {
-    res.render("new", { category });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-})
+router.get("/new", (req, res) => {
+  Categories.find()
+    .lean()
+    .sort({ name: "asc" })
+    .then((category) => {
+      res.render("new", { category });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 router.get("/:id/edit", (req, res) => {
-  const id=req.params.id
+  const id = req.params.id;
   Records.findById(id)
     .lean()
     .then((record) => {
-      const categoryId=record.categoryId
+      const categoryId = record.categoryId;
+      // modify mongoose time into Node.js, this format is dd/mm/yyyy
+      const date = record.date.toLocaleDateString(["ban", "id"]);
+      // change the date format into yyyy-mm-dd, in order to fit input value's format
+      let [dd, mm, yyyy] = date.split("/");
+      if (Number(dd) < 10) {
+        dd = "0" + dd;
+      }
+      if (Number(mm) < 10) {
+        mm = "0" + mm;
+      }
+      const dateArr = [yyyy, mm, dd];
+      const dateValue = dateArr.join("-");
+      record.date = dateValue;
+      console.lo(record.date);
       Categories.findById(categoryId)
         .lean()
         .then((category) => {
-          const categoryName = category.name
+          const categoryName = category.name;
           Categories.find()
             .lean()
             .sort({ name: "asc" })
