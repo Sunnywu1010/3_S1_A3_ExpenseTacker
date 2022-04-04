c90bf060706266d7d15428cec00ede14eb9529f0
Sunny <wuszuyu791010@gmail.com>
2022-04-04 20:11:47 +1000

    feat: add edit page

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,7 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const Categories = require("../../models/categories");
+const Records = require("../../models/records");
 
+router.get("/new",(req,res)=>{
+Categories.find()
+  .lean()
+  .sort({ name: "asc" })
+  .then((category) => {
+    res.render("new", { category });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+})
 router.get("/:id/edit", (req, res) => {
-  res.render("edit",);
+  const id=req.params.id
+  Records.findById(id)
+    .lean()
+    .then((record) => {
+      const categoryId=record.categoryId
+      Categories.findById(categoryId)
+        .lean()
+        .then((category) => {
+          const categoryName = category.name
+          Categories.find()
+            .lean()
+            .sort({ name: "asc" })
+            .then((category) => {
+              res.render("edit", { record, categoryName, category });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 module.exports = router;
